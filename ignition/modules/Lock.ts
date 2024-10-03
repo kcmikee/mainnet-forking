78aaa667a1344a89b8952e0461732debe0bf7ad8
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-10-04 00:18:37 +0100

    init

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -3,15 +3,17 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI: bigint = 1_000_000_000n;
 
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+const UseRemoveLiquidityModule = buildModule(
+  "UseRemoveLiquidityModule",
+  (m) => {
+    const router = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    const lock = m.contract("UseRemoveLiquidity", [router]);
 
-  return { lock };
-});
+    return { lock };
+  }
+);
 
-export default LockModule;
+export default UseRemoveLiquidityModule;
+
+// https://sepolia-blockscout.lisk.com//address/0xeE4b7475D7bDaB1dD07364eB944CC6Ad025b646C#code
